3c92661ece55821e7010d4bcaed8c8d664c18c6b
Saswata Rautray <47337257+saswatarautray@users.noreply.github.com>
2022-10-19 00:33:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
     r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    
+    
     if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
